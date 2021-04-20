d8a1a64a585dc07e904785659ceadd377cb1fa25
andyukra <andyguachin87@gmail.com>
2021-04-20 16:07:21 -0300

    v 0.0.2

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,17 @@
 import mongoose from 'mongoose';
+const {connect, disconnect} = mongoose;
 
-mongoose.connect('mongodb://localhost:27017/frensocial', {
+connect('mongodb+srv://andyukra:salamaleiku@cluster0.dwsam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(db => console.log('DATABASE OK'))
+    .then(db => {
+        console.log('DATABASE OK');
+        if(process.env['npm_lifecycle_event'] === 'build'){
+            disconnect();
+        }
+    })
     .catch(err => console.log(err));
+
+const x = 'asd';
+export default x;
